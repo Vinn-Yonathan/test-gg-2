0ce2396c46dfadb9c12cdee414053e6fd26b7476
Cevin Albert Yonathan <cevinalbertyonathan@gmail.com>
2024-03-11 10:11:14 +0700

    tambah list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi vitae non illo voluptatem nemo deleniti, repudiandae odio. Nobis, aliquid facilis.</p>
+    <ul>
+        <li>ddd</li>
+        <li>sfss</li>
+    </ul>
 </body>
 </html>
